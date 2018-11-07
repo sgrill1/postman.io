f0176103ae7061b6262023a71f605a3233afed74
Sam Grill <sgrill@spartaglobal.com>
2018-11-07 14:53:34 +0000

    initial commit

---
+++ src/test/java/postcodes/io/UnitTest.java
@@ -0,0 +1,20 @@
+package postcodes.io;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UnitTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
